e4dc124ad1b7dd2672a549122c0adaf81adee891
Chimatapu <ravikchi@gmail.com>
2017-05-30 13:03:18 +0100

    working ok

--- src/main/java/com/ravi/fuzzyToolBox/UserInterface/FXCanvas.java
+++ src/main/java/com/ravi/fuzzyToolBox/UserInterface/FXCanvas.java
@@ -7,11 +7,23 @@ import com.ravi.fuzzyToolBox.FuzzySets.FuzzySetImpl;
 import com.ravi.fuzzyToolBox.Rules.Rules;
 import com.ravi.fuzzyToolBox.Tnorm;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontSmoothingType;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBoundsType;
 import javafx.stage.Stage;
 
 import javax.swing.text.html.HTMLDocument;
@@ -22,20 +34,72 @@ import java.util.*;
  * Created by 611445924 on 29/05/2017.
  */
 public class FXCanvas extends Application {
+    Pane root;
+    Scene scene;
+    Stage defStage;
+
     public void start(Stage primaryStage) throws Exception {
-        int width = 600;
-        int height = 600;
-        int margin = 100;
-        Canvas canvas = new Canvas(width, height);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        final int width = 600;
+        final int height = 600;
+        final int margin = 100;
 
-        gc.setLineWidth(1.0);
+        root = new Pane();
 
-        gc.strokeLine(margin, 0, margin, height);
-        gc.strokeLine(0, height-margin, width, height-margin);
 
+        //root.getChildren().add(getCanvas(width, height, margin, 0, 0));
+        Label spread1L = new Label("Spread for Input1");
+        spread1L.setTranslateX(110);
+        TextField spread1 = new TextField();
+        spread1.setTranslateX(260);
+        spread1.setText("0");
+        spread1.setId("spread1T");
+
+        Label spread2L = new Label("Spread for Input2");
+        spread2L.setTranslateX(110);
+        spread2L.setTranslateY(30);
+        TextField spread2 = new TextField();
+        spread2.setTranslateY(30);
+        spread2.setTranslateX(260);
+        spread2.setId("spread2T");
+        spread2.setText("0");
+
+        Canvas canvas = getCanvas(width, height, margin, Integer.parseInt(spread1.getText()), Integer.parseInt(spread2.getText()));
+        root.getChildren().add(canvas);
+
+        Button btn = new Button();
+        btn.setText("Draw");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                root.getChildren().remove(root.lookup("#canv"));
+                TextField spread1 = (TextField) root.lookup("#spread1T");
+
+
+                root.getChildren().add(getCanvas(width, height, margin, Integer.parseInt(spread1.getText()), Integer.parseInt(spread1.getText())));
+            }
+        });
+
+        root.getChildren().add(spread1L);
+        root.getChildren().add(spread1);
+        root.getChildren().add(spread2L);
+        root.getChildren().add(spread2);
+        root.getChildren().add(btn);
+
+        //root.getChildren().add(new Line(0, margin,   width,   margin));
+        //root.getChildren().add(new Line(margin, 0,   margin,   height));
+        //addRectagles(width, height, margin,Integer.parseInt(spread1.getText()), Integer.parseInt(spread2.getText()), root);
+
+        scene = new Scene(root);
+
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Test");
+        primaryStage.show();
+
+    }
+
+    private void addRectagles(int width, int height, int margin, int spread1, int spread2, Pane root){
         FuzzyPartitions partitions = new FuzzyPartitions();
-        Rules rules = partitions.getRules(new RulesInputs(new FuzzySetImpl(0), new FuzzySetImpl(0)));
+        Rules rules = partitions.getRules(new RulesInputs(new FuzzySetImpl(spread1), new FuzzySetImpl(spread2)));
         FZOperation fzOperation = new Tnorm();
         int[][] counts = partitions.getCount(rules, fzOperation);
 
@@ -48,63 +112,121 @@ public class FXCanvas extends Application {
         while(it.hasNext()){
             int value = it.next();
             List<String> coordinatesli = rectangles.get(value);
-            if(value == 1) {
-                gc.setFill(Color.WHITE);
-            }else if(value == 2){
-                gc.setFill(Color.BLUE);
-            }else if(value == 3){
-                gc.setFill(Color.AQUA);
-            }else if(value == 4){
-                gc.setFill(Color.RED);
-            }else if(value == 6){
-                gc.setFill(Color.BEIGE);
-            }else if(value == 9){
-                gc.setFill(Color.MAGENTA);
-            }
-
             for(String coordinates:coordinatesli) {
                 String[] arr = coordinates.split(",");
-                double x1 =Integer.parseInt(arr[0])*10+100;
-                double y1 = Integer.parseInt(arr[1])*10;
-                double x2 = Integer.parseInt(arr[2])*10+100;
-                double y2 = Integer.parseInt(arr[3])*10;
+                double x1 =Integer.parseInt(arr[0])*10+margin;
+                double y1 = Integer.parseInt(arr[1])*10+margin;
+                double x2 = Integer.parseInt(arr[2])*10+margin;
+                double y2 = Integer.parseInt(arr[3])*10+margin;
 
                 double w = Math.abs(x2 - x1);
                 double h = Math.abs(y2 - y1);
 
-                gc.fillRect(x1, y1, w, h);
+                Rectangle rec = new Rectangle(x1, y1, w, h);
+                Text text = new Text(value+"");
+                text.setFontSmoothingType(FontSmoothingType.LCD);
+                text.setX(x1+w/2);
+                text.setY(y1+h/2);
+                if(value == 1) {
+                    rec.setFill(Color.WHITE);
+                }else if(value == 2){
+                    rec.setFill(Color.BLUE);
+                }else if(value == 3){
+                    rec.setFill(Color.AQUA);
+                }else if(value == 4){
+                    rec.setFill(Color.RED);
+                }else if(value == 6){
+                    rec.setFill(Color.BEIGE);
+                }else if(value == 9){
+                    rec.setFill(Color.MAGENTA);
+                }
+
+                root.getChildren().add(rec);
+                root.getChildren().add(text);
             }
         }
+    }
 
-        Pane root = new Pane();
-        root.getChildren().add(canvas);
+    private Canvas getCanvas(int width, int height, int margin, int spread1, int spread2){
+        Canvas canvas = new Canvas(width, height);
+        canvas.setId("canv");
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        Scene scene = new Scene(root);
+        gc.setLineWidth(1.0);
 
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Test");
-        primaryStage.show();
+        gc.strokeLine(margin, 0, margin, height);
+        gc.strokeLine(0, margin, width, margin);
 
+        FuzzyPartitions partitions = new FuzzyPartitions();
+        Rules rules = partitions.getRules(new RulesInputs(new FuzzySetImpl(spread1), new FuzzySetImpl(spread2)));
+        FZOperation fzOperation = new Tnorm();
+        int[][] counts = partitions.getCount(rules, fzOperation);
+
+        this.printResults(counts);
+        Map<Integer, List<String>> rectangles = this.findRectangles(counts);
+
+
+
+        Iterator<Integer> it = rectangles.keySet().iterator();
+        while(it.hasNext()){
+            int value = it.next();
+            List<String> coordinatesli = rectangles.get(value);
+
+
+            for(String coordinates:coordinatesli) {
+                String[] arr = coordinates.split(",");
+                double x1 =Integer.parseInt(arr[0])*10+100;
+                double y1 = Integer.parseInt(arr[1])*10+100;
+                double x2 = Integer.parseInt(arr[2])*10+100;
+                double y2 = Integer.parseInt(arr[3])*10+100;
+
+                double w = Math.abs(x2 - x1);
+                double h = Math.abs(y2 - y1);
+
+                setGcFill(value, gc);
+                gc.fillRect(x1, y1, w, h);
+                gc.setFill(Color.BLACK);
+                gc.fillText(value+"", x1+w/2, y1+h/2);
+            }
+        }
+        return canvas;
+    }
+
+    private void setGcFill(int value, GraphicsContext gc){
+        if(value == 1) {
+            gc.setFill(Color.WHITE);
+        }else if(value == 2){
+            gc.setFill(Color.BLUE);
+        }else if(value == 3){
+            gc.setFill(Color.AQUA);
+        }else if(value == 4){
+            gc.setFill(Color.RED);
+        }else if(value == 6){
+            gc.setFill(Color.BEIGE);
+        }else if(value == 9){
+            gc.setFill(Color.MAGENTA);
+        }
     }
 
     public Map<Integer, List<String>>  findRectangles(int[][] counts){
-        int curNum = 0;
+        int curNum = 1;
         Map<Integer, List<Triplet>> rowSE = new HashMap<Integer, List<Triplet>>();
 
         for (int i = 0; i <counts.length ; i++) {
             curNum = 0;
             int oldEnd = 0;
             for (int j = 0; j <counts[i].length ; j++) {
+                int value = counts[i][j];
                 if(counts[i][j] != curNum){
                     List<Triplet> endPoints = rowSE.get(curNum);
                     if(endPoints == null){
                         endPoints = new ArrayList<Triplet>();
                         rowSE.put(curNum, endPoints);
                     }
-                    endPoints.add(new Triplet(i,oldEnd,(j-1) ));
+                    endPoints.add(new Triplet(i,oldEnd,j));
 
                     curNum = counts[i][j];
-                    oldEnd = j-1;
+                    oldEnd = j;
                 }
             }
             List<Triplet> endPoints = rowSE.get(curNum);
@@ -143,11 +265,15 @@ public class FXCanvas extends Application {
                 List<Integer> ll = listMap.get(coordinates);
                 Collections.sort(ll);
                 String[] arr = coordinates.split("~");
-                if(Integer.parseInt(arr[0])<0 || Integer.parseInt(arr[1])<1){
+                if(Integer.parseInt(arr[0])<0 || Integer.parseInt(arr[1])<0){
                     continue;
                 }
 
-                int startI = ll.get(0)-1;
+                int startI = ll.get(0);
+                if(ll.get(0) != 0){
+                    startI = startI-1;
+                }
+
                 int prevI = ll.get(0);
                 for (int i = 1; i < ll.size(); i++) {
                     if(prevI+1!=ll.get(i)){
@@ -163,7 +289,7 @@ public class FXCanvas extends Application {
 
 
                     }
-                    prevI = ll.get(i)+1;
+                    prevI = ll.get(i);
                 }
                 System.out.println("coordinates for "+k+" : ("+arr[0]+", "+startI+") and ("+arr[1]+", "+prevI+")");
                 List<String> coords = output.get(k);
