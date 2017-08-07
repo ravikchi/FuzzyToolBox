d85b0baed372bb26356fda0ea34c0d56450efc20
Chimatapu <ravikchi@gmail.com>
2017-08-07 16:37:16 +0100

    latest changes

--- src/main/java/com/ravi/fuzzyToolBox/UserInterface/FuzzyUI.java
+++ src/main/java/com/ravi/fuzzyToolBox/UserInterface/FuzzyUI.java
@@ -852,8 +852,8 @@ public class FuzzyUI extends Application {
 
         inputs.clear();
         if(inputTypeText.equalsIgnoreCase("2") && spread1>0 && spread2>0) {
-            inputs.add(new FuzzySetImpl(spread1,spread1/10));
-            inputs.add(new FuzzySetImpl(spread2, spread2/10));
+            inputs.add(new FuzzySetImpl(spread1-(2*spread1/10),spread1/10));
+            inputs.add(new FuzzySetImpl(spread2-(2*spread2/10), spread2/10));
         }else {
             inputs.add(new FuzzySetImpl(spread1, trapezoidal));
             inputs.add(new FuzzySetImpl(spread2, trapezoidal));
